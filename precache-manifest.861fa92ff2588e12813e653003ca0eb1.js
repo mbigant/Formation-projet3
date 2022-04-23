5c2d009a0c7ab59fe5dd19ef93a4e15ac62ed507
mbigant <mickael.bigant@protonmail.com>
2022-04-23 17:43:39 +0200

    Updates

---
+++ precache-manifest.861fa92ff2588e12813e653003ca0eb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3e2366d6f60b5766954ca8fcf4d144f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4412a3073071438a6b8",
+    "url": "/static/css/2.37c457eb.chunk.css"
+  },
+  {
+    "revision": "3934d80b3bd3c0a62783",
+    "url": "/static/css/main.8e1f2138.chunk.css"
+  },
+  {
+    "revision": "c4412a3073071438a6b8",
+    "url": "/static/js/2.c0633e2b.chunk.js"
+  },
+  {
+    "revision": "3934d80b3bd3c0a62783",
+    "url": "/static/js/main.8cbe789f.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
